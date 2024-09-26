5a92d34d3ba4d0834f50b38a619fea5908e26d3c
mukeshdhadhariya <mukeshdhadhariya1@gmail.com>
2024-09-26 23:42:35 +0530

    Context api

---
+++ src/Context/Theme.js
@@ -0,0 +1,13 @@
+import { useContext,createContext } from "react";
+
+export const ThemeContext = createContext({
+    thememode:"light",
+    darktheme:()=>{}, 
+    lighttheme:()=>{},
+})
+
+export const Themeprovider=ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(Themeprovider)
+}
